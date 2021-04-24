06be61b23d95a8483782739f5aba51011750f498
Maksim <maksim.dorofeev.95@mail.ru>
2021-04-24 15:51:12 +0300

    Формулы

---
+++ InTouch2021/InTouch2021/AverageValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InTouch2021
+{
+	public class AverageValue
+	{
+		RatingInformation information = new RatingInformation();
+		RatingInvalid Invalid = new RatingInvalid();
+		RatingService service = new RatingService();
+		public static int NumberUser = 0;
+
+		float SumInf = 0;
+		float SumInval= 0;
+		float SumServ = 0;
+
+		public float AverageInformation()
+		{
+			SumInf += (information.RuleOne() + information.RuleSecond() + information.RuleThird());
+			return SumInf / NumberUser;
+		}
+		public float AverageInvalid()
+		{
+			SumInval += (Invalid.RuleOne() + Invalid.RuleSecond() + Invalid.RuleThird());
+			return SumInval / NumberUser;
+		}
+		public float AverageService()
+		{
+			SumServ += (service.RuleOne() + service.RuleSecond());
+			return SumServ / NumberUser;
+		}
+	}
+}
